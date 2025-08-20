725ec12049e64cf9a33cbdb88f6f686ef2e2e4fd
brayan-r1 <brayan.rivas2026@hilasal.superate.org.sv>
2025-08-19 21:22:16 -0600

    LandingPageTeacher

---
+++ src/LandingPageTeacher.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./CSS/LandingPageTeacher.css";
+
+export function Landingpage() {
+  return (
+    <div>
+      <div className="conteiner">
+       
+        <div className="fondo"></div>
+        <div className="logo">
+          <h1>
+            Teach<span className="highlight">Me</span><span className="highlight2">Now</span>
+          </h1>
+        </div>
+
+        <header className="Barra_busqueda">
+          <input type="text" placeholder="Pregúntame..." />
+          <button>Enviar</button>
+        </header>
+
+        
+        <main className="main-content">
+          <div className="Cards">
+            <div className="Card">
+              <img
+                src="Teacher.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="card-body">
+              <h2>Docentes</h2>
+              <p>Inicia con estudiantes y mejora tus clases</p>
+              <button>Iniciar</button>
+            </div>
+          </div>
+
+          <div className="Cards">
+            <div className="Card">
+              <img
+                src="Alumno.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="card-body">
+              <h2>Estudiantes</h2>
+              <p>Inicia con tus estudios y mejora tus habilidades</p>
+              <button>Iniciar</button>
+            </div>
+          </div>
+
+          <div className="Cards">
+            <div className="Card">
+              <img
+                src="Director.jpeg"
+                alt=""
+              />
+            </div>
+            <div className="card-body">
+              <h2>Director</h2>
+              <p>Inicia como director y mejora la administración de la escuela</p>
+              <button>Iniciar</button>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Footer } from './Footer'
+import { Landingpage } from './LandingPageTeacher'
 import App from './App'
 import { Navbar } from './navbar'
 
@@ -9,5 +9,6 @@ createRoot(document.getElementById('root')).render(
   <>
   <Navbar/>
   <App />
+  <Landingpage />
   </>
 )
