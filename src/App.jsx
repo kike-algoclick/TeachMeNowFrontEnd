21375e601492640d1fbc9c9e06c16ecaf6bc875a
roreyes0 <rociodelosangelesgallaghermarr@gmail.com>
2025-08-22 15:55:04 -0600

    Resultados alumnos added

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,28 @@ import Login from "./Login";
 import { Test } from "./testEstudiante.jsx";
 import { Preguntas } from "./Cuestionario.jsx";
 import Signup from './Signup'
-import {MainPage} from './MainPageT.jsx'
-import {BrowserRouter,Routes, Route} from 'react-router-dom'
 import { Resultados } from "./Resultados.jsx";
-
-
-
-
 
 function App() {
     return (
       <>
+<<<<<<< Updated upstream
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
         </Routes>
+=======
+        <Login />
+        <Signup/>
+        <div className="App">
+          <Test/>
+          <Preguntas/>
+          <Footer />
+          <Resultados/>
+        </div>
+      
+>>>>>>> Stashed changes
 
           <Test />
           <Preguntas />
